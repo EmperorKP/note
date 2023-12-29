55d588f31ff6623a3855f3ff3ed5d9556e505095
KRISHNA PRASATH <142793664+EmperorKP@users.noreply.github.com>
2023-12-29 13:12:28 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Notepad from "./components/Editor";
+
+function App() {
+  return (
+    <div className="App">
+      <Notepad />
+    </div>
+  );
+}
+
+export default App;
